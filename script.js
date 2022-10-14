78de7d952a5a6a072bfb3b36d31834c1324568d2
andrephelipe <andre.phelipe.1999@gmail.com>
2022-10-14 15:53:25 -0300

    ex 03

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 window.onload = function () {
-    const btnForm = document.querySelector('btnSubmit');
-    const inpuEmail = document.getElementById('email');
-    const inputSenha = document.getElementById('senha');
-    btnForm.addEventListener('click', function () {
-
-        if (inpuEmail.innerText === 'tryber@teste.com' && inputSenha.innerText === '123456') {
-            alert('Olá, Tryber!');
-        }else {
-            alert('Email ou senha inválidos.');
-        }
-    });
+  const btnForm = document.getElementById('btnSubmit');
+  const inputEmail = document.getElementById('email');
+  const inputSenha = document.getElementById('senha');
+  btnForm.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Email ou senha inválidos.');
+    }
+    console.log(inputEmail.value);
+    console.log(inputSenha.value);
+    console.log('oi');
+  });
 };
