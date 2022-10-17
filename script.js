e84c702651f80459b4ce17e01ca3adcf1b771de9
ricardokuhlkamp <ric.kds@hotmail.com>
2022-10-17 10:48:52 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btnForm = document.getElementById('btnSubmit');
 const inputEmail = document.getElementById('email');
 const inputSenha = document.getElementById('senha');
+const agree = document.getElementById('agreement');
+const btnSubmit = document.getElementById('submit-btn');
 btnForm.addEventListener('click', (e) => {
   e.preventDefault();
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -11,3 +13,8 @@ btnForm.addEventListener('click', (e) => {
   console.log(inputEmail.value);
   console.log(inputSenha.value);
 });
+
+
+agree.addEventListener('change', function() {
+  btnSubmit.disabled = !agree.checked;
+})
