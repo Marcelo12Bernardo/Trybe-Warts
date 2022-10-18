195ff642e929e0e21d17316fde35f3f49619bb4d
ricardokuhlkamp <ric.kds@hotmail.com>
2022-10-18 10:35:00 -0300

    adicionando os inputs name, lastName, email, house e family ao localStorage

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const btnSubmit = document.getElementById('submit-btn');
 const showCounter = document.getElementById('counter');
 const textArea = document.getElementById('textarea');
 
+const firstName = document.getElementById('input-name');
+const lastName = document.getElementById('input-lastname');
+const email = document.getElementById('input-email');
+const house = document.getElementById('house');
+const family = document.getElementById('familia');
+
 btnForm.addEventListener('click', (e) => {
   e.preventDefault();
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -23,3 +29,10 @@ textArea.addEventListener('input', () => {
   const text = textArea.value.length;
   showCounter.innerHTML = 500 - text;
 });
+// marco
+// adição dos inputs no localStotage:
+firstName.addEventListener('input', () => localStorage.setItem('keyFirstName', firstName.value));
+lastName.addEventListener('input', () => localStorage.setItem('keyLastName', lastName.value));
+email.addEventListener('input', () => localStorage.setItem('keyEmail', email.value));
+house.addEventListener('change', (e) => localStorage.setItem('keyHouse', e.target.value));
+family.addEventListener('click', (e) => localStorage.setItem('keyFamily', e.target.value));
